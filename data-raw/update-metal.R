7eab3e65e430b1e7551c4755437da4339b330172
Dewey Dunnington <dewey@fishandwhistle.net>
2022-10-01 22:26:25 -0300

    with include!

---
+++ data-raw/update-metal.R
@@ -0,0 +1,17 @@
+
+curl::curl_download(
+  "https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip",
+  "data-raw/metal.zip"
+)
+unzip("data-raw/metal.zip", exdir = "data-raw")
+
+withr::with_dir("data-raw/metal-cpp", {
+  system("python3 ./SingleHeader/MakeSingleHeader.py Foundation/Foundation.hpp QuartzCore/QuartzCore.hpp Metal/Metal.hpp")
+})
+
+unlink("src/Metal", recursive = T)
+dir.create("src/Metal")
+file.copy("data-raw/metal-cpp/SingleHeader/Metal.hpp", "src/Metal/Metal.hpp")
+
+unlink("data-raw/metal.zip")
+unlink("data-raw/metal-cpp", recursive = TRUE)
--- src/code.cpp
+++ src/code.cpp
@@ -1,5 +1,7 @@
 #include <cpp11.hpp>
 using namespace cpp11;
 
+#include "Metal/Metal.hpp"
+
 [[cpp11::register]]
 void fun() {}
